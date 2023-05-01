f5a5c2bdf3f2fa4e946670950044cab32c72bf04
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-05-01 14:34:55 +0100

    Installed npm packages [Ticket: X]

--- utils/redis.js
+++ utils/redis.js
@@ -1,1 +1,45 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
+// This class defines methods for the redis client
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to server: ${error}`);
+    });
+  }
+
+  // checks the connection status and reports
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  // get the value for the given key from redis server
+  async get(key) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
+  }
+
+  // set the key value pair to redis server
+  async set(key, value, time) {
+    const redisSet = promisify(this.client.set).bind(this.client);
+    await redisSet(key, value);
+    await this.client.expire(key, time);
+  }
+
+  // del key vale pair from redis server
+  async del(key) {
+    const redisDel = promisify(this.client.del).bind(this.client);
+    await redisDel(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+//module.exports = redisClient;
+export default redisClient;
