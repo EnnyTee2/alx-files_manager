488f271a963ab0202a6f235cda48bf04c7ea91bf
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-05-01 22:05:27 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -3,11 +3,11 @@ import { MongoClient } from 'mongodb';
 const HOST = process.env.DB_HOST || 'localhost';
 const PORT = process.env.DB_PORT || 27017;
 const DATABASE = process.env.DB_DATABASE || 'files_manager';
-const db_url = `mongodb://${HOST}:${PORT}`;
+const dbUrl = `mongodb://${HOST}:${PORT}`;
 
 class DBClient {
   constructor() {
-    this.client = new MongoClient(db_url, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.client = new MongoClient(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true });
     this.client.connect().then(() => {
       this.db = this.client.db(`${DATABASE}`);
     }).catch((err) => {
